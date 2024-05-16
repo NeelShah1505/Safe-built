7f3d75671ffd9ee1239d4521a94b0dbf1472c6db
guboc11 <guboc11@gmail.com>
2024-05-17 08:43:52 +0900

    Add basic functions

--- src/icp-make-canister-backend/src/index.ts
+++ src/icp-make-canister-backend/src/index.ts
@@ -1,7 +1,144 @@
-import { Canister, query, text } from 'azle';
+// import { Canister,  query, update, nat64, text } from 'azle';
+import {
+    bool,
+    Canister,
+    nat64,
+    nat8,
+    Opt,
+    query,
+    StableBTreeMap,
+    text,
+    Tuple,
+    update,
+    Vec,
+    Record,
+    Some,
+    None,
+    Void
+} from 'azle';
+
+const Key = nat8;
+type Key = typeof Key.tsType;
+
+const Value = text;
+type Value = typeof Value.tsType;
+
+let map = StableBTreeMap<Key, Value>(0);
+
+//================================================
+
+
+const File = Record({
+    title : text,
+    contents : text 
+});
+type File = typeof File.tsType;
+
+let fileMap = StableBTreeMap<text, File>(0);
+
+const User = Record({
+    id : text,
+    pw : text
+});
+type User = typeof User.tsType;
+
+let userMap = StableBTreeMap<text, User>(0);
+
+let fileAccessLog : Vec<text> = [];
+
+
+
+let logIndex: nat64 = 0n;
 
 export default Canister({
-    greet: query([text], text, (name) => {
-        return `Hello, ${name}!`;
-    })
+    uploadFile: update([File, text], bool, (file, userId) => {
+        if (!userMap.containsKey(userId)) {
+            return false
+        }
+        if (!fileMap.containsKey(file.title)) {
+            fileMap.insert(file.title, file);
+            return true
+        }
+        return false
+    }),
+    readFile: update([text, text], Opt(File), (title, userId) => {
+        if (!userMap.containsKey(userId)) {
+            return None
+        }
+        if (fileMap.get(title) != None) {
+            fileAccessLog.push(`${logIndex} ${userId} read ${title}`);
+            logIndex += 1n;
+        }
+        return fileMap.get(title)
+    }),
+    getFileAccessLog: query([], Vec(text), () => {
+        return fileAccessLog
+    }),
+    register: update([User], bool, (user) => {
+        if (!userMap.containsKey(user.id)) {
+            userMap.insert(user.id, user);
+            return true
+        };
+        return false
+    }),
+    login: query([User], bool, (user) => {
+        if (userMap.containsKey(user.id)) {
+            let u = userMap.get(user.id);
+            if (u.Some?.pw == user.pw) {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    }),
+
+    
+    // greet: query([text], text, (name) => {
+    //     return `Hello, ${name}!`;
+    // }),
+    // readCount: query([], nat64, () => {
+    //     return count;
+    // }),
+    // incrementCount: update([], nat64, () => {
+    //     count += 1n;
+
+    //     return count;
+    // }),
+    // containsKey: query([Key], bool, (key) => {
+    //     return map.containsKey(key);
+    // }),
+
+    // get: query([Key], Opt(Value), (key) => {
+    //     return map.get(key);
+    // }),
+
+    // insert: update([Key, Value], Opt(Value), (key, value) => {
+    //     return map.insert(key, value);
+    // }),
+
+    // isEmpty: query([], bool, () => {
+    //     return map.isEmpty();
+    // }),
+
+    // items: query([], Vec(Tuple(Key, Value)), () => {
+    //     return map.items();
+    // }),
+
+    // keys: query([], Vec(Key), () => {
+    //     return Uint8Array.from(map.keys());
+    // }),
+
+    // len: query([], nat64, () => {
+    //     return map.len();
+    // }),
+
+    // remove: update([Key], Opt(Value), (key) => {
+    //     return map.remove(key);
+    // }),
+
+    // values: query([], Vec(Value), () => {
+    //     return map.values();
+    // })
 })
--- src/icp-make-canister-frontend/src/App.jsx
+++ src/icp-make-canister-frontend/src/App.jsx
@@ -1,29 +1,119 @@
 import { useState } from 'react';
 import { icp_make_canister_backend } from 'declarations/icp-make-canister-backend';
 
-function App() {
-  const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
+
+
+function App() {
+  const [message, setMessage] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
+  const [userId, setUserId] = useState('');
+  // const [userPw, setUserPw] = useState('');
+  const [fileContents, setFileContents] = useState('');
+
+
+  function registerUser(event) {
     event.preventDefault();
-    const name = event.target.elements.name.value;
-    icp_make_canister_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
+    const userId = event.target.elements.register_id.value;
+    const userPw = event.target.elements.register_pw.value;
+    icp_make_canister_backend.register({id: userId, pw: userPw}).then((isRegistered) => {
+      setMessage(`user is registered? : ${isRegistered}`);
     });
     return false;
-  }
+  };
+
+  function login(event) {
+    event.preventDefault();
+    const userId = event.target.elements.login_id.value;
+    const userPw = event.target.elements.login_pw.value;
+    icp_make_canister_backend.login({id: userId, pw: userPw}).then((isLogin) => {
+      if (isLogin){
+        setUserId(userId)
+      } else {
+        setUserId('');
+      };
+      setIsLogin(isLogin);
+    });
+  };
+
+  function uploadFile(event) {
+    event.preventDefault();
+    const fileTitle = event.target.elements.upload_file_title.value;
+    const fileContents = event.target.elements.upload_file_contents.value;
+    if (isLogin) {
+      icp_make_canister_backend.uploadFile({title: fileTitle, contents: fileContents}, userId).then((isUploaded) => {
+        console.log("isUploaded? ",isUploaded )
+      });
+    };
+   
+  };
+  
+  function readFile(event) {
+    event.preventDefault();
+    const fileTitle = event.target.elements.read_file_title.value;
+    if (isLogin) {
+      icp_make_canister_backend.readFile(fileTitle, userId).then((file) => {
+        if (file.length > 0) {
+          console.log("file",file);
+          setFileContents(file[0].contents);
+        } else {
+          setFileContents('');
+        }
+      });
+    } else {
+      console.log("can't read file")
+    }
+  };
 
   return (
     <main>
       <img src="/logo2.svg" alt="DFINITY logo" />
       <br />
       <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      {/* <div>
+        <p>login? {isLogin}</p>
+        <p>user? {userId}</p>
+      </div> */}
+      <div>
+        <p>Register User</p>
+        <form action="#" onSubmit={registerUser}>
+          <label htmlFor="register_id">ID : &nbsp;</label>
+          <input id="register_id" alt="ID" type="text" />
+          <label htmlFor="register_pw">PW : &nbsp;</label>
+          <input id="register_pw" alt="PW" type="text" />
+          <button type="submit">REGISTER</button>
+        </form>
+        <section id="message">{message}</section>
+      </div>
+      <div>
+        <p>{`User Login? ${isLogin} User Id? ${userId}`}</p>
+        <form action="#" onSubmit={login}>
+          <label htmlFor="login_id">ID : &nbsp;</label>
+          <input id="login_id" alt="ID" type="text" />
+          <label htmlFor="login_pw">PW : &nbsp;</label>
+          <input id="login_pw" alt="PW" type="text" />
+          <button type="submit">LOGIN</button>
+        </form>
+      </div>
+      <div>
+        <p>Upload File</p>
+        <form action="#" onSubmit={uploadFile}>
+          <label htmlFor="upload_file_title">Title : &nbsp;</label>
+          <input id="upload_file_title" alt="Title" type="text" />
+          <label htmlFor="upload_file_contents">Contents : &nbsp;</label>
+          <input id="upload_file_contents" alt="Contents" type="text" />
+          <button type="submit">UPLOAD</button>
+        </form>
+      </div>
+      <div>
+        <p>Read File</p>
+        <form action="#" onSubmit={readFile}>
+          <label htmlFor="read_file_title">Title : &nbsp;</label>
+          <input id="read_file_title" alt="Title" type="text" />
+          <button type="submit">READ</button>
+        </form>
+        <section id="file_contents">{`file contents : ${fileContents}`}</section>
+      </div>
     </main>
   );
 }
