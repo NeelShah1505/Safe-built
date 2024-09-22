2abb21893a7e12ab63f96eea30a52d8c004f73c6
guboc11 <guboc11@gmail.com>
2024-09-22 23:43:45 +0900

    Add skeleton of functions

--- src/BlockHero_backend/src/lib.rs
+++ src/BlockHero_backend/src/lib.rs
@@ -3,49 +3,76 @@ use ic_cdk::{query, update};
 use std::cell::RefCell;
 
 thread_local! {
-    static COUNTER: RefCell<Nat> = RefCell::new(Nat::from(0 as u32));
+  static COUNTER: RefCell<Nat> = RefCell::new(Nat::from(0 as u32));
 }
+
+// user 관련
+#[update]
+fn register_user() {
+
+}
+
+#[query]
+fn check_user_registered() -> bool {
+  true
+}
+
+#[query]
+fn login() -> bool {
+  // login에 필요한 값 주고, login 성공? 실패?
+  true
+}
+
+// file 관련
+#[update]
+fn upload_file() {
+
+}
+
+#[query]
+fn read_file() {
+}
+
+#[update] // 이건 구현 x
+fn update_file() {
+
+}
+
+#[update]
+fn delete_file() {
+
+}
+
+fn create_log() {
+
+}
+
+#[query]
+fn read_logs() {
+  
+}
+
+
+
+
+
+
 
 /// Get the value of the counter.
 #[query]
 fn get() -> Nat {
-    COUNTER.with(|counter| counter.borrow().clone())
+  COUNTER.with(|counter| counter.borrow().clone())
 }
 
 /// Set the value of the counter.
 #[update]
 fn set(n: Nat) {
-    COUNTER.with(|counter| *counter.borrow_mut() = n);
+  COUNTER.with(|counter| *counter.borrow_mut() = n);
 }
 
 #[update]
 fn increment() {
-    COUNTER.with(|counter| *counter.borrow_mut() += 1 as u32);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_get_set() {
-        let expected = Nat::from(42 as u32);
-        set(expected.clone());
-        assert_eq!(get(), expected);
-    }
-
-    #[test]
-    fn test_init() {
-        assert_eq!(get(), Nat::from(0 as u32));
-    }
-
-    #[test]
-    fn test_inc() {
-        for i in 1..10 {
-            increment();
-            assert_eq!(get(), Nat::from(i as u32));
-        }
-    }
+  COUNTER.with(|counter| *counter.borrow_mut() += 1 as u32);
 }
 
 ic_cdk::export_candid!();
