eb7c31ca4c045f557ad46662d07a5c42c7211b70
jeffreyham523 <jeffreyham523@gmail.com>
2024-11-06 19:00:04 +0900

    Create React App

---
+++ Frontend/frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="text-xs mx-auto" >
+      block block
+     </div>
+     <div>
+      hi
+     </div>
+    </div>
+  );
+}
+
+export default App;
