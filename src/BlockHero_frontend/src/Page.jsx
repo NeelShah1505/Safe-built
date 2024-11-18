791376fbcb4a264ea47470165450be99150bde7c
guboc11 <guboc11@gmail.com>
2024-11-18 22:25:02 +0900

    Add Pages & Install tailwindcss

---
+++ src/BlockHero_frontend/src/Page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page() {
+    return (
+        <div className="container mx-auto p-4 max-w-lg">
+            <h1 className="text-3xl font-bold">Page Component</h1>
+            <p className="mt-4">이곳은 새로운 페이지입니다.</p>
+        </div>
+    );
+}
+
+export default Page;
