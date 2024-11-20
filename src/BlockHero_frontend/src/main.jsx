e498cca929fab296e87cd59fa2cd6acd88a669f5
guboc11 <guboc11@gmail.com>
2024-11-20 23:04:52 +0900

    Add UserContext

--- src/BlockHero_frontend/src/main.jsx
+++ src/BlockHero_frontend/src/main.jsx
@@ -1,13 +1,19 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { IdentityProvider } from './IdentityContext';
+import { UserProvider } from './UserContext';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <HashRouter>
+      <IdentityProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </IdentityProvider>
+    </HashRouter>,
     document.getElementById('root')
 );
